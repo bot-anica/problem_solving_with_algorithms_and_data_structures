2fc334e4f1e7157dfa7a1e4e5c2f3e67d1697300
Bot Anica <test.develop.user@gmail.com>
2025-11-07 18:53:43 +0200

    Solve task 2 of the chapter

---
+++ 2_basic_data_structures/tasks/task_2.py
@@ -0,0 +1,81 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
+
+    def __str__(self):
+        return f"{",".join(self.items)}"
+
+
+# 2. Измените алгоритм вычисления постфиксного выражения таким образом, чтобы он обрабатывал ошибки.
+
+
+def default_token_validation(token):
+    if token.isalpha():
+        raise ValueError(
+            "Expression shoul includes only integer numbers and math operators: +, -, *, /, ^")
+
+    if "." in token:
+        raise ValueError("You can use only integer numbers.")
+
+    # -1, -30 and so on
+    if len(token) >= 2 and token[0] == "-" and token[1].isnumeric():
+        raise ValueError("You can use only numbers more than 0.")
+
+
+def postfix_eval(postfix_expr):
+    op_stack = Stack()
+    tokens_list = postfix_expr.split()
+
+    for token in tokens_list:
+        default_token_validation(token)
+
+        if token.isnumeric():
+            op_stack.push(int(token))
+        else:
+            if op_stack.size() >= 2:
+                op2 = op_stack.pop()
+                op1 = op_stack.pop()
+                math_result = do_math(token, op1, op2)
+                op_stack.push(math_result)
+            else:
+                raise ValueError("There is one or more extra mathematical operators. You should remove extra math operators or add missing operands (numbers).")
+
+    if op_stack.size() > 1:
+        raise ValueError("There is one or more extra operands (numbers). You should remove extra operands (numbers) or add missing math operators.")
+
+    return op_stack.pop()
+
+
+def do_math(op, op1, op2):
+    if op == "+":
+        return op1 + op2
+    elif op == "-":
+        return op1 - op2
+    elif op == "*":
+        return op1 * op2
+    elif op == "/":
+        if op2 == 0:
+            raise ValueError("Division by zero is prohibited.")
+        return op1 / op2
+    elif op == "^":
+        return op1 ** op2
+    else:
+        raise ValueError("You used unknown math operator. You can use only +, -, *, / and ^.")
+
+
+print(postfix_eval('7 8 + 3 2 + /'))
