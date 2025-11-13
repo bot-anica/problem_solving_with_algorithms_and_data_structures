c4e8cdd8708c9bd334fda77b7f36f41e0e6e93c3
Bot Anica <test.develop.user@gmail.com>
2025-11-13 16:38:09 +0200

    Solve task 13 in the chapter

---
+++ 2_basic_data_structures/tasks/task_13.py
@@ -0,0 +1,129 @@
+# 13. В реализации метода `size` мы подсчитываем количество узлов в списке.
+# Альтернативной стратегией было бы хранить количество узлов
+# в дополнительном поле данных в голове списка.
+#
+# Измените класс UnorderedList, чтобы включить в него эту информацию,
+# и перепишите метод size.
+
+from data_structures import Node
+
+
+class UnorderedList:
+    def __init__(self):
+        self.head = None
+        self.rear = None
+        self.list_size = 0
+
+    def is_empty(self):
+        return self.head is None
+
+    def add(self, item):
+        new_node = Node(item)
+        new_node.set_next(self.head)
+
+        if self.rear is None:
+            self.rear = new_node
+
+        self.head = new_node
+        self.list_size += 1
+
+    def size(self):
+        return self.list_size
+
+    def search(self, item):
+        found = False
+        current = self.head
+        while not (current is None) and not found:
+            if current.get_data() == item:
+                found = True
+            else:
+                current = current.get_next()
+        return found
+
+    def remove(self, item):
+        found = False
+        previous = None
+        current = self.head
+        while not found and not (current is None):
+            if current.get_data() == item:
+                found = True
+            else:
+                previous = current
+                current = current.get_next()
+
+        if found:
+            self.list_size -= 1
+            if previous is None:
+                self.head = current.get_next()
+            else:
+                previous.set_next(current.get_next())
+
+    def append(self, item):
+        new_node = Node(item)
+        self.list_size += 1
+        if self.rear is None:
+            self.rear = new_node
+            self.head = new_node
+        else:
+            self.rear.set_next(new_node)
+            self.rear = new_node
+
+    def insert(self, item, index):
+        current_index = 0
+        previous = None
+        current = self.head
+        while current_index < index and not (current is None):
+            previous = current
+            current = current.get_next()
+            current_index += 1
+
+        new_node = Node(item)
+        self.list_size += 1
+        if self.head is None:
+            self.head = new_node
+        elif current is None:
+            previous.set_next(new_node)
+        else:
+            new_node.set_next(current)
+            previous.set_next(new_node)
+
+    def index(self, item):
+        found = False
+        current_index = 0
+        current = self.head
+        while not found and not (current is None):
+            if current.get_data() == item:
+                found = True
+            else:
+                current = current.get_next()
+                current_index += 1
+
+        if found:
+            return current_index
+        else:
+            return -1
+
+    def pop(self, index = None):
+        found = False
+        current_index = 0
+        previous = None
+        current = self.head
+        while not found and not (current is None):
+            if current_index == index:
+                found = True
+            else:
+                previous = current
+                current = current.get_next()
+                current_index += 1
+
+        target_item = current.get_data() if current else previous.get_data() if previous else None
+        if target_item:
+            self.list_size -= 1
+            if not (self.head is None) and previous is None:
+                self.head = current.get_next()
+            elif index is None:
+                previous.set_next(None)
+            elif found:
+                previous.set_next(current.get_next())
+
+        return target_item
